728626c32f794b6bbb2da6913be7bdab16cadbcf
gilsonDias92 <gilsinho.1992@gmail.com>
2020-02-06 16:04:53 -0300

    creating a separated file for the application routes

--- heroway-facebook-react/src/components/App.tsx
+++ heroway-facebook-react/src/components/App.tsx
@@ -2,25 +2,12 @@ import React from "react";
 import Header from "./Header";
 import Main from "./Main";
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./Main/Login";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-      <Route exact={true} path="/">
-          <Login />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/feed">
-          <Main />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Routes />
   );
 }
 
---
+++ heroway-facebook-react/src/components/Routes.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Header from "./Header";
+import Login from "./Main/Login";
+import Main from "./Main";
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact={true} path="/">
+          <Login />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/feed">
+          <Main />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
